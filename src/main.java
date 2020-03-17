47e8719ec85f49fd252c086a8c6a16385f999a67
Codel1417 <austinrichards98@gmail.com>
2020-03-17 11:10:25 -0700

    first commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+final String HandbrakeDir "C:\\Program Files\\HandBrake\\HandBrake.exe";
+
+    public static void main(String[] args){
+        //get list of files
+        //itterate using foreach
+            //compare against txt
+            //if not on list
+                //convert video
+                //write dir of new file to txt
+                // delete old file
+    }
+}
