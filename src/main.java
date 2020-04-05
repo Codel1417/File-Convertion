0c93af8ad76486966e4b6969fbeb6c0e107b89e8
Codel1417 <austinrichards98@gmail.com>
2020-04-04 18:03:54 -0700

    Update main.java

--- src/main.java
+++ src/main.java
@@ -18,7 +18,7 @@ final static String convertedTxt = "converted.txt";
         ArrayList<String> filetxt = readFile(new File(convertedTxt));
         try {
             //walk the file tree finding all video files
-            filelist = Files.walk(Paths.get("Y:\\Shows\\Arrow"))
+            filelist = Files.walk(Paths.get("Y:\\Movies"))
                     .filter(Files::isRegularFile)
                     .collect(Collectors.toList());
 
@@ -30,19 +30,19 @@ final static String convertedTxt = "converted.txt";
 
                     for (String file : filetxt){
                         String filePath = path.toFile().getAbsolutePath().trim();
-                        System.out.println("Comparing " + filePath + " to " + file);
+                        //System.out.println("Comparing " + filePath + " to " + file);
                         if (file.trim().contains(filePath)){
                             fileWasConverted = true;
                             break;
                         }
                     }
                     if (fileWasConverted){
-                        System.out.println(" || Converted File Found");
+                        //System.out.println(" || Converted File Found");
                         break;
                     }
                     else{
                         //add found video to conversion queue
-                        System.out.println(" || Unconverted File Found");
+                        //System.out.println(" || Unconverted File Found");
                         fileQueue.add(path.toFile());
                     }
                 }
@@ -61,6 +61,7 @@ final static String convertedTxt = "converted.txt";
 
                 //create exec command
                 String outputFile = file.getAbsolutePath().substring(0,file.getAbsolutePath().length()-3) + "mp4";
+                outputFile = outputFile.replaceAll("2160","1080");
                 String execCommand ="\"" +  HandbrakeDir + "\" -i \"" + newfile.getAbsolutePath() + "\" -o \"" + outputFile + "\" -f av_mp4 --inline-parameter-sets --markers --optimize -e nvenc_h265 --vfr --all-subtitles --encoder-level auto -q 22 -B 160 --arate auto --aencoder copy --all-audio --maxHeight 1080 --maxWidth 1920 --keep-display-aspect  --encoder-preset slow ";
                 System.out.println("Exec: "+ execCommand);
                 //Process p = Runtime.getRuntime().exec(execCommand);
@@ -80,9 +81,7 @@ final static String convertedTxt = "converted.txt";
                     newfile.renameTo(file);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
 
